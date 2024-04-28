f7dfe5ce6f4009de86de0843472f75740087b90e
tri <trikaydev@gmail.com>
2024-04-28 17:59:45 +0700

    Initial Commit

---
+++ Messages d'AmourNEW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Messages d'AmourNEW
+//
+//  Created by Trí on 4/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Messages d'AmourNEW/Messages_d_AmourNEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messages_d_AmourNEWApp.swift
+//  Messages d'AmourNEW
+//
+//  Created by Trí on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Messages_d_AmourNEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
